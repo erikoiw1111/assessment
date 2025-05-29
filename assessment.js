8f3529cc9fb59aa46569b5c63d0d567eda867190
erikoiw1111 <eriko.iw1111@gmail.com>
2025-05-29 14:01:11 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -48,7 +48,7 @@ assessmentButton.onclick = () => { //アロー関数表記。assessmentButton.on
     anchor.setAttribute('href',hrefValue);
     anchor.className='twitter-share-button';//anchor.setAttribute('class','twitter-share-button');でもOK
     anchor.setAttribute('data-text',result);
-    anchor.setAttribute('innerText','#あなたのいいところ');
+    anchor.setAttribute('inner-Text','#あなたのいいところ');
     anchor.setAttribute('data-lang', 'ja');
     anchor.setAttribute('data-show-count', 'false');
     tweetDivided.appendChild(anchor);
